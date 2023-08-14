c8148057c4b58b5697efab68d59d1f16c3a87465
lpascarelli <luca.pascarelli@brocc.se>
2023-08-14 16:13:08 +0200

    Add support for locales in Contentful

--- app/[locale]/news/page.tsx
+++ app/[locale]/news/page.tsx
@@ -1,13 +1,32 @@
-import Section from '@/components/ui/section';
+import { EntryFieldTypes } from 'contentful';
+import { cookies } from 'next/headers';
 
-export default function NewsPage() {
+import Section from '@/components/ui/section';
+import { LOCALES } from '@/constants/languages';
+import { getOrCreateClient } from '@/utils';
+
+interface NewsSkeleton {
+  contentTypeId: 'news';
+  fields: {
+    news: EntryFieldTypes.Text;
+    title: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+  };
+}
+
+export default async function NewsPage() {
+  const locale = cookies().get('NEXT_LOCALE');
+  const client = getOrCreateClient();
+  const { items } = await client.getEntries<NewsSkeleton>({
+    content_type: 'news',
+    locale: LOCALES[locale?.value as keyof typeof LOCALES] || 'en-US',
+  });
+  const { title, description } = items[0].fields;
+
   return (
     <Section>
-      <p className='text-center'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quod
-        consequuntur corporis molestiae aliquam sed in nisi quos impedit eum
-        asperiores omnis qui nulla ea, ut earum numquam nemo accusantium?
-      </p>
+      <h1 className='text-center text-2xl font-bold'>{title}</h1>
+      <p className='text-center'>{description}</p>
     </Section>
   );
 }
--- constants/languages/index.ts
+++ constants/languages/index.ts
@@ -1,1 +1,6 @@
 export const LANGUAGES = ['en', 'es', 'sv'];
+
+export const LOCALES = {
+  'en': 'en-US',
+  'es': 'es-AR'
+};
