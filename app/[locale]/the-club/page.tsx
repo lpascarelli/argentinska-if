d83f41dca024e0e8855b8b26c7b485edfc44f735
lpascarelli <luca.pascarelli@brocc.se>
2023-08-30 15:06:46 +0200

    Refactor for getting data from Contentful (next refactor- API nextJS usage)

--- app/[locale]/the-club/page.tsx
+++ app/[locale]/the-club/page.tsx
@@ -1,41 +1,39 @@
-import { cookies } from 'next/headers';
-
 import TheClub from '@/components/the-club';
-import { LOCALES } from '@/constants/languages';
+import { getEntry } from '@/contentful-client';
 import {
   TheClubSkeleton,
   ManagementSkeleton,
 } from '@/interfaces/contentful-api';
 import { getYear } from '@/helpers';
 import { History, Management } from '@/interfaces/the-club';
-import { getOrCreateClient } from '@/utils';
 
 export default async function ClubPage() {
-  const locale = cookies().get('NEXT_LOCALE');
-  const client = getOrCreateClient();
-  const theClub = await client.getEntries<TheClubSkeleton>({
-    content_type: 'theClub',
-    locale: LOCALES[locale?.value as keyof typeof LOCALES] || 'en-US',
-  });
-  const managementSkeleton = await client.getEntries<ManagementSkeleton>({
-    content_type: 'management',
-    locale: LOCALES[locale?.value as keyof typeof LOCALES] || 'en-US',
-  });
+  const [theClubSkeleton, managementSkeleton] = await Promise.all([
+    getEntry<TheClubSkeleton>('theClub'),
+    getEntry<ManagementSkeleton>('management'),
+  ]);
 
-  const history: History = {
-    title: theClub.items[0].fields.historyTitle,
-    image: theClub.items[0].fields.historyContent.content[0].data.target,
-    textNodes: theClub.items[0].fields.historyContent.content.slice(1),
-  };
-
-  const management: Management[] = managementSkeleton.items.map((item) => {
-    return {
-      name: item.fields.title,
-      year: getYear(item.fields.dateOfBirth),
-      introduction: item.fields.introduction.toString(),
-      avatar: item.fields.manager,
+  if (theClubSkeleton.ok && managementSkeleton.ok) {
+    const history: History = {
+      title: theClubSkeleton.data.items[0].fields.historyTitle,
+      image:
+        theClubSkeleton.data.items[0].fields.historyContent.content[0].data
+          .target,
+      textNodes:
+        theClubSkeleton.data.items[0].fields.historyContent.content.slice(1),
     };
-  });
 
-  return <TheClub history={history} management={management} />;
+    const management: Management[] = managementSkeleton.data.items.map(
+      (item) => {
+        return {
+          name: item.fields.title,
+          year: getYear(item.fields.dateOfBirth),
+          introduction: item.fields.introduction.toString(),
+          avatar: item.fields.manager,
+        };
+      }
+    );
+
+    return <TheClub history={history} management={management} />;
+  }
 }
